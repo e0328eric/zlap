b96fc2507dfe743d568208393eb8eb6dc095898c
Sungbae Jeong <almagest0328@gmail.com>
2023-02-04 15:47:02 +0900

    change build script supporting zig v0.11.0-beta

--- build.zig
+++ build.zig
@@ -2,15 +2,28 @@ const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
-    const strip = b.option(bool, "strip", "Omit debug information") orelse false;
+    const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary("zlap", "src/zlap.zig");
-    lib.setBuildMode(mode);
+    const version = std.builtin.Version{
+        .major = 0,
+        .minor = 1,
+        .patch = 0,
+    };
+
+    const lib = b.addStaticLibrary(.{
+        .name = "zlap",
+        .root_source_file = .{ .path = "src/zlap.zig" },
+        .target = target,
+        .optimize = optimize,
+        .version = version,
+    });
     lib.install();
 
-    const main_tests = b.addTest("src/zlap.zig");
-    main_tests.setBuildMode(mode);
+    const main_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/zlap.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
@@ -20,10 +33,13 @@ pub fn build(b: *std.build.Builder) void {
         .source = .{ .path = "src/zlap.zig" },
     };
 
-    const example_exe = b.addExecutable("zlap-example", "example/main.zig");
-    example_exe.strip = strip;
-    example_exe.setTarget(target);
-    example_exe.setBuildMode(mode);
+    const example_exe = b.addExecutable(.{
+        .name = "zlap-example",
+        .root_source_file = .{ .path = "example/main.zig" },
+        .target = target,
+        .optimize = optimize,
+        .version = version,
+    });
     example_exe.addPackage(zlap);
     example_exe.install();
 
