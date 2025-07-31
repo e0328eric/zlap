9650e1ad4daca66c7c25a56b8bd628f12bcc2a62
Sungbae Jeong <almagest0328@gmail.com>
2025-07-31 20:04:09 +0900

    foo

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-const MIN_ZIG = std.SemanticVersion.parse("0.14.0-dev.3213+53216d2f2") catch unreachable;
+const MIN_ZIG = std.SemanticVersion.parse("0.14.1") catch unreachable;
 
 // NOTE: This code came from
 // https://github.com/zigtools/zls/blob/master/build.zig.
@@ -22,14 +22,12 @@ pub fn build(b: *Build) void {
 
     const version = std.SemanticVersion.parse("0.6.1") catch unreachable;
 
-    const lib = b.addStaticLibrary(.{
-        .name = "zlap",
+    const zlap_mod = b.addModule("zlap", .{
         .root_source_file = b.path("src/zlap.zig"),
         .target = target,
         .optimize = optimize,
         .version = version,
     });
-    b.installArtifact(lib);
 
     const main_tests = b.addTest(.{
         .root_source_file = b.path("src/zlap.zig"),
@@ -40,16 +38,14 @@ pub fn build(b: *Build) void {
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
 
-    const zlap_mod = b.addModule("zlap", .{
-        .root_source_file = b.path("src/zlap.zig"),
-    });
-
     const example_exe = b.addExecutable(.{
         .name = "zlap-example",
-        .root_source_file = b.path("example/main.zig"),
-        .target = target,
-        .optimize = optimize,
-        .version = version,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("example/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .version = version,
+        }),
     });
     example_exe.root_module.addImport("zlap", zlap_mod);
     b.installArtifact(example_exe);
